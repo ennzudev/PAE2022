1720e4719a131a4cb6ab391f83985d346584394e
Pablo <nunezp20@gmail.com>
2022-11-08 01:55:32 -0600

    update tarea1

--- assets/scripts/index.js
+++
@@ -1,9 +0,0 @@
-let quantity = 12
-
-var elements = "";
-for (var i = 0; i < quantity; i++) {
-   elements += '<div class="element"></div>';
-}
-var panel = document.getElementById("panel");
-panel.innerHTML = elements;
-
